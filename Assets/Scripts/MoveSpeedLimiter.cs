51222bea16c7dd8a9a2b4d45c9ee18118512997f
Yves <yvesprinz59@gmail.com>
2023-09-13 20:07:12 +0200

    Ok, controllers found
    We now correctly find and confirm controllers in this script, ready to be used for all sorts of shenanigans. mainly to check what the inputs are.

--- Assets/Scripts/MoveSpeedLimiter.cs
+++ Assets/Scripts/MoveSpeedLimiter.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class MoveSpeedLimiter : MonoBehaviour
 {
     private bool controllersChecked = false;
+    private bool controllersAssigned = false;
+    private bool tempCheck = false;
+    
+    private InputDevice leftController;
+    private InputDevice rightController;
 
-    public List<UnityEngine.XR.InputDevice> inputDevices;
+    List<UnityEngine.XR.InputDevice> leftControllers = new List<InputDevice>();
+    List<UnityEngine.XR.InputDevice> rightControllers = new List<InputDevice>();
+
     void Start()
     {
         ControllerCountEnsurance();
@@ -14,12 +22,19 @@ public class MoveSpeedLimiter : MonoBehaviour
 
     void ControllerCheck() //checks what controllers are connected
     {
-        inputDevices = new List<UnityEngine.XR.InputDevice>();
-        UnityEngine.XR.InputDevices.GetDevices(inputDevices);
+        InputDeviceCharacteristics leftControllerCharacteristics = InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.TrackedDevice | InputDeviceCharacteristics.Left;
+        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.TrackedDevice | InputDeviceCharacteristics.Right;
+        InputDevices.GetDevicesWithCharacteristics(leftControllerCharacteristics, leftControllers);
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, rightControllers);
+        //these should fill with one controller each. one for the left and one for the right.
 
-        foreach (var device in inputDevices)
+        foreach (var device in leftControllers)
         {
-            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.characteristics.ToString()));
+            Debug.Log(string.Format("Device found with name '{0}' and characteristics '{1}'", device.name, device.characteristics.ToString()));
+        }
+        foreach (var device in rightControllers)
+        {
+            Debug.Log(string.Format("Device found with name '{0}' and characteristics '{1}'", device.name, device.characteristics.ToString()));
         }
     }
     
@@ -27,7 +42,7 @@ public class MoveSpeedLimiter : MonoBehaviour
     {
         ControllerCheck();
 
-        if (inputDevices.Count <= 2)
+        if (leftControllers.Count + rightControllers.Count < 1)
         {
             Debug.LogWarning("Problem: missing controllers! check if they're all connected pls");
         }
@@ -35,14 +50,44 @@ public class MoveSpeedLimiter : MonoBehaviour
         {
             controllersChecked = true;
         }
+        //as long as at least one controller is connected, we'll be fine
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (controllersChecked == false) //this will make sure enough controllers are connected before starting to play/run the rest of the code
+        if (!controllersChecked) //this will make sure enough controllers are connected before starting to play/run the rest of the code
         {
             ControllerCountEnsurance();
+        }
+
+        if (controllersChecked && (!controllersAssigned) && (leftControllers.Count  + rightControllers.Count >= 1))
+        {
+            foreach (var device in leftControllers)
+            {
+                Debug.Log("we're putting " + device + " in as the left controller");
+                leftController = device;
+            }
+            foreach (var device in rightControllers)
+            {
+                Debug.Log("we're putting " + device + " in as the right controller");
+                rightController = device;
+            }
+
+            Debug.Log("[In forEach loop check] our left controller should be: " + leftController.characteristics.ToString() + " And our right controller should be: " + rightController.characteristics.ToString());
+
+            if ((leftController != null) && (rightController != null))
+            {
+                controllersAssigned = true;
+            }
+        }
+
+        
+
+        if (controllersAssigned && (!tempCheck))
+        {
+            Debug.Log("[normal check] our left controller should be: " + leftController.characteristics.ToString() + " And our right controller should be: " + rightController.characteristics.ToString());
+            tempCheck = true;
         }
 
     }
